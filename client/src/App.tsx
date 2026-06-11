edefb1828fcbf9e85146b24804f08f0a83a9dedb
Ajay <bharnawaajay246@gmail.com>
2026-06-11 19:39:29 +0530

    feat: setup frontend architecture and tailwind

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-slate-950 text-white flex items-center justify-center">
+      <h1 className="text-5xl font-bold">
+        Frontend is up and running!
+      </h1>
+    </div>
+  )
+}
+
+export default App
